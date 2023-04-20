50661a98abe792a3fd511587c05f5b1c2da5fd34
raisbinuli <rais27@MSTLPT210801IT.mensa-group.com>
2023-04-20 14:10:48 +0700

    add product service and order service

---
+++ go-grpc-order-svc/cmd/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"order_service/pkg/client"
+	"order_service/pkg/config"
+	"order_service/pkg/db"
+	"order_service/pkg/pb"
+	service "order_service/pkg/services"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	c, err := config.LoadConfig()
+
+	if err != nil {
+		log.Fatalln("Failed at config", err)
+	}
+
+	h := db.Init(c.DBUrl)
+
+	lis, err := net.Listen("tcp", c.Port)
+
+	if err != nil {
+		log.Fatalln("Failed to listing:", err)
+	}
+
+	productSvc := client.InitProductServiceClient(c.ProductSvcUrl)
+
+	if err != nil {
+		log.Fatalln("Failed to listing:", err)
+	}
+
+	fmt.Println("Order Svc on", c.Port)
+
+	s := service.Server{
+		H:          h,
+		ProductSvc: productSvc,
+	}
+
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterOrderServiceServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalln("Failed to serve:", err)
+	}
+}
---
+++ go-grpc-product-svc/cmd/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"product_service/pkg/config"
+	"product_service/pkg/db"
+	pb "product_service/pkg/pb"
+	services "product_service/pkg/services"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	c, err := config.LoadConfig()
+
+	if err != nil {
+		log.Fatalln("Failed at config", err)
+	}
+
+	h := db.Init(c.DBUrl)
+
+	lis, err := net.Listen("tcp", c.Port)
+
+	if err != nil {
+		log.Fatalln("Failed to listing:", err)
+	}
+
+	fmt.Println("Product Svc on", c.Port)
+
+	s := services.Server{
+		H: h,
+	}
+
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterProductServiceServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalln("Failed to serve:", err)
+	}
+}
